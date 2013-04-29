042d4171fb25420b4525303dfd93f59a1595545b
Justin Merz <jrmerz@gmail.com>
2013-04-29 13:45:45 -0700

    another quick fix to export data uri

--- web/server/public/js/chart.js
+++ web/server/public/js/chart.js
@@ -57,12 +57,12 @@ ESIS.chart = (function(){
 	
 	// TODO: need to test in more browsers
 	function _createExportBtn(name, xName, yName, arr) {
-		var url = "data:application/csv;charset=utf-8,";
+		var url = "data:application/csv;charset=UTF-8,";
 		var data = xName+","+yName+"\n";
 		for( var i = 0; i < arr.length; i++ ) {
 			data += arr[i][0]+","+arr[i][1]+"\n";
 		}
-		$("#result-export").html("<a class='btn' download='"+name+".csv' href='"+url+encodeURIComponent(data)+"'>Download CSV</a>");
+		$("#result-export").html("<a class='btn' target='_blank' download='"+name+".csv' href='"+url+encodeURIComponent(data)+"'>Download CSV</a>");
 		
 	}
 	
