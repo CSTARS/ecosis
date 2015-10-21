1018bf266aadd9f412268e6dcf494d9412b13ad5
Justin Merz <jrmerz@gmail.com>
2015-10-21 09:42:23 -0700

    format fix

--- models/lookup.js
+++ models/lookup.js
@@ -28,8 +28,12 @@ function findKey(query, callback) {
       result = arr;
 
       result.sort(function(a, b){
-        if( a.toLowerCase() > b.toLowerCase() ) return 1;
-        if( a.toLowerCase() < b.toLowerCase() ) return -1;
+        if( a.toLowerCase() > b.toLowerCase() ) {
+          return 1;
+        }
+        if( a.toLowerCase() < b.toLowerCase() ) {
+          return -1;
+        }
         return 0;
       });
       callback(null, result);
