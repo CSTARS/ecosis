0130008e45b92ec070e52044ccd760f35133adf5
Justin Merz <jrmerz@gmail.com>
2015-11-03 11:05:18 -0800

    fixes for script

--- scripts/install_p2.sh
+++ scripts/install_p2.sh
@@ -10,27 +10,27 @@ fi
 
 echo "Continuing install..."
 
-echo "Starting jetty"
-sudo service jetty start
+echo "**** Starting jetty"
+sudo service jetty8 start
 
-echo "Replacing SOLR default schema with CKAN schema"
+echo "**** Replacing SOLR default schema with CKAN schema"
 sudo mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
 sudo ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema.xml
 
-echo "Restarting jetty"
+echo "**** Restarting jetty"
 sudo service jetty restart
 
-echo "Creating PG tables"
+echo "**** Creating PG tables"
+. /usr/lib/ckan/default/bin/activate
 cd /usr/lib/ckan/default/src/ckan
 paster db init -c /etc/ckan/default/development.ini
 
-echo "Linking who.ini"
+echo "**** Linking who.ini"
 ln -s /usr/lib/ckan/default/src/ckan/who.ini /etc/ckan/default/who.ini
 
-echo "Starting CKAN"
+echo "**** Starting CKAN"
 echo ". /usr/lib/ckan/default/bin/activate"
+echo "cd /usr/lib/ckan/default/src/ckan"
 echo "paster serve /etc/ckan/default/development.ini"
 
-. /usr/lib/ckan/default/bin/activate
-cd /usr/lib/ckan/default/src/ckan
 paster serve /etc/ckan/default/development.ini
