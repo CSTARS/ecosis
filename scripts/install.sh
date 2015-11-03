b6956abbca9c5805b9d4dd36a1ef8c27b39aee57
Justin Merz <jrmerz@gmail.com>
2015-11-03 10:46:54 -0800

    adding install scripts

---
+++ scripts/install.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+# Basically summing up: http://docs.ckan.org/en/latest/maintaining/installing/install-from-source.html
+
+VERSION=2.4.1
+
+echo ""
+echo "You are about to install CKAN v$VERSION."
+echo "Do you wish to continue? [Y/n]"
+
+read -n 1 answer
+if [ $answer != "Y" ]; then
+        echo "bye bye"
+        exit
+fi
+
+echo "Installing packages"
+sudo apt-get install python-dev postgresql libpq-dev python-pip python-virtualenv git-core solr-jetty openjdk-6-jdk
+
+echo "Create Python virtual env"
+sudo mkdir -p /usr/lib/ckan/default
+sudo chown `whoami` /usr/lib/ckan/default
+virtualenv --no-site-packages /usr/lib/ckan/default
+. /usr/lib/ckan/default/bin/activate
+
+echo "Installing v$VERSION of CKAN"
+githuburl="git+https://github.com/ckan/ckan.git@ckan-$VERSION#egg=ckan"
+pip install -e $githuburl
+
+echo "Installing CKAN pip requirements"
+pip install -r /usr/lib/ckan/default/src/ckan/requirements.txt
+
+echo "Reactivating Python virtualenv"
+deactivate
+. /usr/lib/ckan/default/bin/activate
+
+echo "Creating CKAN Postgres User"
+sudo -u postgres createuser -S -D -R -P ckan_default
+
+echo "Creating CKAN Postgres database"
+sudo -u postgres createdb -O ckan_default ckan_default -E utf-8
+
+echo "Init CKAN config"
+sudo mkdir -p /etc/ckan/default
+sudo chown -R `whoami` /etc/ckan/
+
+cd /usr/lib/ckan/default/src/ckan
+paster make-config ckan /etc/ckan/default/development.ini
+
+echo "As far as I go.  You need to do the following:"
+echo "Edit /var/lib/ckan/default.init"
+echo " - update pg connection: sqlalchemy.url = postgresql://ckan_default:pass@localhost/ckan_default"
+echo " - update solr connection: solr_url=http://127.0.0.1:8983/solr"
+echo " - update site id: ckan.site_id = default"
+echo " - update site url: ckan.site_url = http://demo.ckan.org"
+echo ""
+echo "Edit /etc/default/jetty"
+echo " - NO_START=0"
+echo " - JETTY_HOST=127.0.0.1"
+echo " - JETTY_PORT=8983"
+echo ""
+echo "Once this is done, run install_p2.sh"
---
+++ scripts/install_p2.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+echo "Do you wish to continue? [Y/n]"
+
+read -n 1 answer
+if [ $answer != "Y" ]; then
+        echo "bye bye"
+        exit
+fi
+
+echo "Continuing install..."
+
+echo "Starting jetty"
+sudo service jetty start
+
+echo "Replacing SOLR default schema with CKAN schema"
+sudo mv /etc/solr/conf/schema.xml /etc/solr/conf/schema.xml.bak
+sudo ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema.xml
+
+echo "Restarting jetty"
+sudo service jetty restart
+
+echo "Creating PG tables"
+cd /usr/lib/ckan/default/src/ckan
+paster db init -c /etc/ckan/default/development.ini
+
+echo "Linking who.ini"
+ln -s /usr/lib/ckan/default/src/ckan/who.ini /etc/ckan/default/who.ini
+
+echo "Starting CKAN"
+echo ". /usr/lib/ckan/default/bin/activate"
+echo "paster serve /etc/ckan/default/development.ini"
+
+. /usr/lib/ckan/default/bin/activate
+cd /usr/lib/ckan/default/src/ckan
+paster serve /etc/ckan/default/development.ini
