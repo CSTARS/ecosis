c64175c96792aa4de8ea65d20fbb976a8a87140c
Justin Merz <jrmerz@gmail.com>
2016-02-11 11:00:56 -0800

    quick fix

--- serverConfig/dev.js
+++ serverConfig/dev.js
@@ -2,6 +2,11 @@ var config = require('./index');
 
 config.mqe.dev = true;
 config.mqe.seo = false;
+
+if( !config.mqe.ckan ) {
+  config.mqe.ckan = {};
+}
+
 config.mqe.ckan.server = 'http://dev-data.ecospectra.org';
 config.mqe.server.url = 'http://dev-search.ecospectra.org';
 module.exports = config;
--- serverConfig/prod.js
+++ serverConfig/prod.js
@@ -2,6 +2,11 @@ var config = require('./index');
 
 config.mqe.dev = false;
 config.mqe.server.url = 'http://ecospectra.org';
+
+if( !config.mqe.ckan ) {
+  config.mqe.ckan = {};
+}
+
 config.mqe.ckan.server = 'http://data.ecospectra.org';
 config.mqe.server.googleAnalytics = 'UA-60014992-1';2
 module.exports = config;
